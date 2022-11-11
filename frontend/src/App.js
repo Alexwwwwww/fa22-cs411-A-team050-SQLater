359c3f80d074f8056dcb16d0ee12714eaf42d713
AJ1245 <abheerjasuja@gmail.com>
2022-11-11 12:56:15 -0600

    Added the Update and Delete command

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ function App() {
   const [enteredUIN, setEnteredUIN] = useState("");
   const [enteredHW_id, setHW_ID] = useState("");
   const [enteredHW_Name, setHW_Name] = useState("");
+  const [updateHW_id, setupdateHW_ID] = useState("");
+  const [updateHW_Name, setupdateHW_Name] = useState("");
+  const [deleteHW_id, setdeleteHW_id] = useState("");
 
   const uinChangeHandler = async (event) => {
     setEnteredUIN(event.target.value);
@@ -19,9 +22,63 @@ function App() {
     console.log(event.target.value); 
   };
 
+  const updatehw_idChangleHandler = async (event) => {
+    setupdateHW_ID(event.target.value);
+    console.log(event.target.value); 
+  };
+  const deletehw_idChangleHandler = async (event) => {
+    setdeleteHW_id(event.target.value);
+    console.log(event.target.value); 
+  };
+
+
   const HW_NameChangleHandler= async (event) => {
     setHW_Name(event.target.value);
     console.log(event.target.value);
+  };
+
+  const updateHW_NameChangleHandler= async (event) => {
+    setupdateHW_Name(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const SubmitDeleteHandler = async (event) => {
+    event.preventDefault();
+
+    console.log("Sending DELETE API CALL");
+
+    const data = {
+      hw_id: deleteHW_id,
+    };
+    axios({
+      method: "DELETE",
+      url: "http://127.0.0.1:5000/deleteHW",
+      data: data,
+    }).then((response) => {
+        console.log(response.data);
+      });
+    setdeleteHW_id("")
+  
+  };
+
+  const SubmitUpdateHandler = async (event) => {
+    event.preventDefault();
+
+    console.log("Sending PUT API CALL");
+
+    const data = {
+      hw_id: updateHW_id,
+      hw_name:updateHW_Name,
+    };
+    axios({
+      method: "PUT",
+      url: "http://127.0.0.1:5000/updateHW",
+      data: data,
+    }).then((response) => {
+        console.log(response.data);
+      });
+    setupdateHW_ID("")
+    setupdateHW_Name("")
   };
 
   const SubmitInsertHandler = async (event) => {
@@ -123,6 +180,42 @@ function App() {
             ></input>
             <button type="submit">Submit</button>
           </label>
+        </form>
+      </div>
+      <div>
+        <h3>Update</h3>
+        <form onSubmit={SubmitUpdateHandler}>
+          <label>
+            Enter the Hw_id:
+            <input
+            type="text"
+            value={updateHW_id}
+            onChange={updatehw_idChangleHandler}        
+            ></input>
+          </label>
+          <label>
+          &nbsp;&nbsp;Enter the HW_Name:
+            <input
+            type="text"
+            value={updateHW_Name}
+            onChange={updateHW_NameChangleHandler}
+            ></input>
+            <button type="submit">Submit</button>
+          </label>
+        </form>
+      </div>
+      <div>
+        <h3>Delete</h3>
+        <form onSubmit={SubmitDeleteHandler}>
+          <label>
+            Enter the Hw_id:
+            <input
+            type="text"
+            value={deleteHW_id}
+            onChange={deletehw_idChangleHandler}        
+            ></input>
+          </label>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </>
